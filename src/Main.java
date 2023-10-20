a2df4fd6d77e525d3bd0325e71e9944b1eecdcb1
Andrey Gavrilov <NewHardyMan09@gmail.com>
2023-10-20 18:10:14 +0200

    row 9

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world");
         File info = new File("INFO.txt");
         ArrayList<String> users = new ArrayList<>();
         try {
